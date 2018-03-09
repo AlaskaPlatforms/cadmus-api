1b4f0daaf954a4d7f7e54186a31e6f9afd93e486
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2018-03-09 20:13:57 -0300

    Adicionar configuração da API

---
+++ config/app.js
@@ -0,0 +1,37 @@
+'use strict'
+const express = require('express')
+const Cadmus = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const consign = require('consign')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const passport = require('passport')
+
+const configEnv = require('./env')
+configEnv(process.env.NODE_ENV)
+
+const passportConfig = require('./passport')
+const config = require('.')
+const database = require('./database')
+
+passportConfig(passport)
+database(mongoose, config)
+
+Cadmus.use(bodyParser.json())
+Cadmus.use(bodyParser.urlencoded({ extended: true }))
+Cadmus.use(morgan('dev'))
+Cadmus.use(cors())
+Cadmus.use(passport.initialize())
+
+Cadmus.set('tokensecret', process.env.SECRET)
+
+consign({ cwd: 'server' })
+  .include('app/setup')
+  .include('app/api')
+  .include('app/routes')
+  .into(Cadmus)
+
+Cadmus.app.setup.index.status()
+
+module.exports = Cadmus
