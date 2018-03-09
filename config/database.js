1181e80317e0555c6fcd5303b62894981ae09a4a
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2018-03-09 20:37:03 -0300

    Adicionar configuração de banco

---
+++ config/database.js
@@ -0,0 +1,17 @@
+'use strict'
+module.export = (mongoose, config) => {
+  const database = mongoose.connection
+  mongoose.Promise = Promise
+  mongoose.connect(config.database, { promiseLibrary: global.Promise })
+
+  database.on('error', error => console.log('Conexão ao banco Cadmus falhou:', error))
+  database.on('connected', () => console.log('Conectado ao banco Cadmus'))
+  database.on('disconnected', () => console.log('Desconectado do banco Cadmus'))
+
+  process.on('SIGINT', () => {
+    database.close(() => {
+      console.log('Banco Cadmus teve a conexão fechada')
+      process.exit(0)
+    })
+  })
+}
