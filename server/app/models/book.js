96779b97661ee3700c4f273afa170fb328721744
NULL-SWEAT <felipe.094@gmail.com>
2018-04-03 20:11:39 -0300

    Description field on Book model added.

--- server/app/models/book.js
+++ server/app/models/book.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema({
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  title: { type: String, required: true }, 
+  title: { type: String, required: true },
+  description: { type: String, required: true },
   chapters: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Chapter' }],
   genre: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Genre' }]
 }, { timestamps: true })
