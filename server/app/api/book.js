1fb427d6f22bad65ec4db4b507e0d60a3f5b97bc
NULL-SWEAT <felipe.094@gmail.com>
2018-04-24 20:29:54 -0300

    Fix usage of async/await on book controller

--- server/app/api/book.js
+++ server/app/api/book.js
@@ -12,9 +12,7 @@ api.addBook = async (req, res) => {
     try {
       const newBook = new Book({ userId, title, description })
       await newBook.save()
-      User.update({ _id: userId }, { $push: { books: newBook._id }}, (err) => {
-        if (err) res.status(400)
-      })
+      await User.update({ _id: userId }, { $push: { books: newBook._id }})
       res.json({ success: true, message: 'Livro criado com sucesso' })
     } catch (error) {
       throw new Error(error)
@@ -23,11 +21,12 @@ api.addBook = async (req, res) => {
   }
 }
 
+// Get books by user's id
 api.getUserBooks = async (req, res) => {
   if (!req.params.id) {
     res.status(400)
   } else {
-    Book.find({ 'userId': req.params.id }, (err, books) => {
+    await Book.find({ 'userId': req.params.id }, (err, books) => {
       if (err) {
         res.status(400)
       } else {
@@ -41,35 +40,38 @@ api.deleteBook = async (req, res) => {
   if (!req.params.id) {
     res.status(400)
   } else {
-    Book.findOne({ '_id': req.params.id }, (err, book) => {
-      if (err) res.status(400)
-      else {
-        // Deleta os capítulos desse livro
-        book.chapters.forEach(chapterId => {
-          Chapter.remove({ '_id': chapterId }, (err) => {
-            if (err) res.status(400)
-          })
-        })
+    try {
+      const { id } = req.params
+      await Book.findOne({ '_id': id }, async (err) => {
+        if (err) res.status(400)
+        else {
+          try {
+            // Deleta os capítulos desse livro
+            await Chapter.remove({ 'bookId': id })
 
-        // Deleta a ref desse livro no User
-        User.update({ books: req.params.id }, { $pull: { books: req.params.id }}, (err) => {
-          if (err) res.status(400)
-        })
+            // Deleta a ref desse livro no User
+            await User.update({ books: id }, { $pull: { books: id }})
 
-        // Deleta o livro
-        Book.remove({ '_id': req.params.id }, (err) => {
-          if (err) res.status(400)
-          else {
+            // Deleta o livro
+            await Book.remove({ '_id': id })
+
             res.status(200).json({ success: true, message: 'Livro deletado com sucesso' })
+          } catch (error) {
+            throw new Error(error)
+            res.status(400)
           }
-        })
-      }
-    })
+        }
+      })
+    } catch (error) {
+      throw new Error(error)
+      res.status(400)
+    }
   }
 }
 
 api.getBooks = async (req, res) => {
   Book.find({}, (err, books) => {
+    if (err) res.status(400)
     res.send(books)
   })
 }
